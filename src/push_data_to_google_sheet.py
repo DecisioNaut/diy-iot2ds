63986202a973b11490ecbee1d068ad280207fed2
QuantificAid <37964284+datorus@users.noreply.github.com>
2019-04-10 16:28:49 +0200

    Work on Chapter 3 (and scripts)

--- src/push_data_to_google_sheet.py
+++ src/push_data_to_google_sheet.py
@@ -1,6 +1,6 @@
 # Import many libraries
-from __future__ import print_function  
-from googleapiclient.discovery import build  
+from __future__ import print_function
+from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from oauth2client.service_account import ServiceAccountCredentials
@@ -13,23 +13,23 @@ MY_CREDENTIALS_FILE = 'NAME OF YOUR JSON CREDENTIAL FILE.json'
 MY_SPREADSHEET_ID = 'NAME OF YOUR GSHEET'
 MY_TAB_NAME = 'Daten'
 
-def datetime_string()
+def datetime_string():
     """datetime_string method:
-        makes a string to push time in the right format to google sheets 
+        makes a string to push time in the right format to google sheets
     """
     now = datetime.datetime.now()
     date = str(now.day) + "." str(now.month) + "." + str(now.year)
     time = str(now.hour) + ":" + str(now.minute) + ":" str(now.second)
     return date + " " + time
 
-def update_sheet(sheetname, temperature, pressure, humidity):  
+def update_sheet(sheetname, temperature, pressure, humidity):
     """update_sheet method:
-       appends a row of a sheet in the spreadsheet with the 
+       appends a row of a sheet in the spreadsheet with the
        the latest temperature, pressure and humidity sensor data
     """
     # Authentication, authorization step
     SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-    creds = ServiceAccountCredentials.from_json_keyfile_name( 
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
             MY_CREDENTIALS_FILE, SCOPES)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
@@ -39,14 +39,14 @@ def update_sheet(sheetname, temperature, pressure, humidity):
     body = { 'values': values }
     # Call the append API to perform the operation
     result = service.spreadsheets().values().append(
-                spreadsheetId=MY_SPREADSHEET_ID, 
+                spreadsheetId=MY_SPREADSHEET_ID,
                 range=sheetname + '!A1:D1',
-                valueInputOption='USER_ENTERED', 
+                valueInputOption='USER_ENTERED',
                 insertDataOption='INSERT_ROWS',
                 body=body
-                ).execute()                     
+                ).execute()
 
-def main():  
+def main():
     """main method:
        reads the BME280 chip to read the three sensors, then
        call update_sheets method to add that sensor data to the spreadsheet
@@ -63,5 +63,5 @@ def main():
     # Update Google Sheet
     update_sheet(MY_TAB_NAME, tempC, pressure, humidity)
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     main()
---
+++ src/stream_weather_data.py
@@ -0,0 +1,5 @@
+import time
+import push_data_to_google_sheet as pdtgs
+
+while True:
+    pdtgs.main()
